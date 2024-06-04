2d522119c47a1f58b167b5f204f0505b425ad1ac
CaptainSkiddow <dekkerrens@outlook.com>
2024-06-04 14:44:24 +0200

    Initial push
    First push. Started working on injecting elements in the HTML using javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+const first_words = [
+    "Fix",
+    "Flip",
+    "Forsee"
+];
+
+const sec_words = [
+    "Fix",
+    "Flip",
+    "Forsee"
+];
+
+const third_words = [
+    "Fix",
+    "Flip",
+    "Forsee"
+];
+
+const span = document.createElement('span');
+const container = document.getElementsByClassName('containerText')[0];
+container.appendChild(span);
